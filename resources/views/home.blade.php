af6647d4d42d090459a4ae5af119063ed8447199
Jess Johannessen <mail@jezz.dk>
2016-07-11 15:22:37 +0200

    Removed noise

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.app')
 
-@section('htmlheader_title')
-	Home
-@endsection
+@section('htmlheader_title', 'Home')
 
+@section('contentheader_title', 'Home')
 
 @section('main-content')
-	<div class="container spark-screen">
-		<div class="row">
-			<div class="col-md-10 col-md-offset-1">
-				<div class="panel panel-default">
-					<div class="panel-heading">Home</div>
 
-					<div class="panel-body">
+		<div class="row">
+			<div class="col-sm-12 col-md-10 col-md-offset-1 col-lg-6 col-lg-offset-3">
+				<div class="box box-default">
+					<div class="box-header with-border">
+						<h3 class="box-title">Home</h3>
+					</div>
+					<div class="box-body">
 						You are logged in!
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
+
 @endsection
--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,8 +2,8 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        <a href="https://github.com/acacha/adminlte-laravel"></a><b>admin-lte-laravel</b></a>. A Laravel 5 package that switchs default Laravel scaffolding/boilerplate to AdminLTE template
+        Mmmm. Bacon.
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2015 <a href="http://acacha.org">Acacha.org</a>.</strong> Created by <a href="http://acacha.org/sergitur">Sergi Tur Badenas</a>. See code at <a href="https://github.com/acacha/adminlte-laravel">Github</a>
+    <strong>Created by <a href="http://nosuchagency.com">NSA</a>. See code at <a href="https://github.com/nosuchagency/beacon-bacon">Github</a>
 </footer>
