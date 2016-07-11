c839d205ef30c79a521848502613c38c61619128
Jess Johannessen <mail@jezz.dk>
2016-07-11 15:45:14 +0200

    Set email sender

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'no-reply@beaconbacon.com', 'name' => 'BeaconBacon'],
 
     /*
     |--------------------------------------------------------------------------
